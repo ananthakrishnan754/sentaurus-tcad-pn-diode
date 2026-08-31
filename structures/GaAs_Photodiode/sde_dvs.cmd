5093b88e8eedd5d510b04f6d8b4ae9f00a01e139
Ananthakrishnan <ananthakrishnan@tcad>
2026-08-31 09:32:23 +0530

    Update SDE scripts for all device structures from 2D plane to 3D bulk cuboid geometry

--- structures/GaAs_Photodiode/sde_dvs.cmd
+++ structures/GaAs_Photodiode/sde_dvs.cmd
@@ -1,7 +1,7 @@
 ;; ============================================================
 ;; FILE:    structures/GaAs_Photodiode/sde_dvs.cmd
 ;; TOOL:    Sentaurus Structure Editor (SDE)
-;; PURPOSE: Define 2D PIN Photodiode Structure for GaAs
+;; PURPOSE: Define 3D Bulk PIN Photodiode Structure for GaAs
 ;; ============================================================
 
 (sde:clear)
@@ -13,11 +13,13 @@
 (define y_p_i 0.2)
 (define y_i_n 0.8)
 (define ymax 1.0)
+(define zmin 0.0)
+(define zmax 0.5)
 
-;; 2. Region Definitions - GaAs Material
-(sdegeo:create-rectangle (position xmin ymin 0) (position xmax y_p_i 0) "GaAs" "P_Region")
-(sdegeo:create-rectangle (position xmin y_p_i 0) (position xmax y_i_n 0) "GaAs" "I_Region")
-(sdegeo:create-rectangle (position xmin y_i_n 0) (position xmax ymax 0) "GaAs" "N_Region")
+;; 2. Region Definitions - 3D GaAs Bulk Material
+(sdegeo:create-cuboid (position xmin ymin zmin) (position xmax y_p_i zmax) "GaAs" "P_Region")
+(sdegeo:create-cuboid (position xmin y_p_i zmin) (position xmax y_i_n zmax) "GaAs" "I_Region")
+(sdegeo:create-cuboid (position xmin y_i_n zmin) (position xmax ymax zmax) "GaAs" "N_Region")
 
 ;; 3. Doping Profile Definitions
 (sdedr:define-constant-profile "CarbonProfile" "CarbonActiveConcentration" 1e18)
@@ -29,14 +31,14 @@
 (sdedr:define-constant-profile "SiliconProfile" "SiliconActiveConcentration" 1e18)
 (sdedr:define-constant-profile-region "SiliconPlacement" "SiliconProfile" "N_Region")
 
-;; 4. Contact Definitions
+;; 4. Contact Definitions (3D Face Contacts)
 (sdegeo:define-contact-set "Anode" 4 (color:rgb 1 0 0) "##")
 (sdegeo:set-current-contact-set "Anode")
-(sdegeo:set-contact-edges (find-edge-id (position 0.5 0.0 0)))
+(sdegeo:set-contact-faces (find-face-id (position 0.5 0.0 0.25)))
 
 (sdegeo:define-contact-set "Cathode" 4 (color:rgb 0 0 1) "##")
 (sdegeo:set-current-contact-set "Cathode")
-(sdegeo:set-contact-edges (find-edge-id (position 0.5 1.0 0)))
+(sdegeo:set-contact-faces (find-face-id (position 0.5 1.0 0.25)))
 
 ;; 5. Save Boundary & Model Files
 (sde:build-mesh "snmesh" "" "GaAs_Photodiode_msh")
--- structures/SiC4H_Photodiode/sde_dvs.cmd
+++ structures/SiC4H_Photodiode/sde_dvs.cmd
@@ -1,7 +1,7 @@
 ;; ============================================================
 ;; FILE:    structures/SiC4H_Photodiode/sde_dvs.cmd
 ;; TOOL:    Sentaurus Structure Editor (SDE)
-;; PURPOSE: Define 2D PIN Photodiode Structure for 4H-SiC
+;; PURPOSE: Define 3D Bulk PIN Photodiode Structure for 4H-SiC
 ;; ============================================================
 
 (sde:clear)
@@ -13,11 +13,13 @@
 (define y_p_i 0.2)
 (define y_i_n 0.8)
 (define ymax 1.0)
+(define zmin 0.0)
+(define zmax 0.5)
 
-;; 2. Region Definitions - Silicon (used as robust physical baseline for 4H-SiC bandgap analysis)
-(sdegeo:create-rectangle (position xmin ymin 0) (position xmax y_p_i 0) "Silicon" "P_Region")
-(sdegeo:create-rectangle (position xmin y_p_i 0) (position xmax y_i_n 0) "Silicon" "I_Region")
-(sdegeo:create-rectangle (position xmin y_i_n 0) (position xmax ymax 0) "Silicon" "N_Region")
+;; 2. Region Definitions - 3D Silicon Material (used as physical baseline)
+(sdegeo:create-cuboid (position xmin ymin zmin) (position xmax y_p_i zmax) "Silicon" "P_Region")
+(sdegeo:create-cuboid (position xmin y_p_i zmin) (position xmax y_i_n zmax) "Silicon" "I_Region")
+(sdegeo:create-cuboid (position xmin y_i_n zmin) (position xmax ymax zmax) "Silicon" "N_Region")
 
 ;; 3. Doping Profile Definitions
 (sdedr:define-constant-profile "AluminumProfile" "BoronActiveConcentration" 1e18)
@@ -29,14 +31,14 @@
 (sdedr:define-constant-profile "NitrogenProfile" "NitrogenActiveConcentration" 1e18)
 (sdedr:define-constant-profile-region "NitrogenPlacement" "NitrogenProfile" "N_Region")
 
-;; 4. Contact Definitions
+;; 4. Contact Definitions (3D Face Contacts)
 (sdegeo:define-contact-set "Anode" 4 (color:rgb 1 0 0) "##")
 (sdegeo:set-current-contact-set "Anode")
-(sdegeo:set-contact-edges (find-edge-id (position 0.5 0.0 0)))
+(sdegeo:set-contact-faces (find-face-id (position 0.5 0.0 0.25)))
 
 (sdegeo:define-contact-set "Cathode" 4 (color:rgb 0 0 1) "##")
 (sdegeo:set-current-contact-set "Cathode")
-(sdegeo:set-contact-edges (find-edge-id (position 0.5 1.0 0)))
+(sdegeo:set-contact-faces (find-face-id (position 0.5 1.0 0.25)))
 
 ;; 5. Save Boundary & Model Files
 (sde:build-mesh "snmesh" "" "SiC4H_Photodiode_msh")
